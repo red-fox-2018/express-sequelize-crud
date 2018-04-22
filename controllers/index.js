e939f4c3a2581f0853c4aee2b31fc8ea1fd1ba7e
Iswanul Umam <umam@sepulsa.com>
2018-04-22 13:40:09 +0700

    finish subject

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 
 const StudentController = require('./student');
 const TeacherController = require('./teacher');
+const SubjectController = require('./subject');
 
 module.exports = {
   StudentController,
-  TeacherController
+  TeacherController,
+  SubjectController
 }
