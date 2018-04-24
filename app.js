810ad11806eed52eb55ba80d61560babb36baabb
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-04-24 10:53:18 +0700

    edit some features

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ app.use('/', homeRoutes);
 let studentRoutes = require('./routes/student.js')
 app.use('/students', studentRoutes)
 
-let addStudentRoutes = require('./routes/addStudent.js')
-app.use('/students/add', addStudentRoutes)
-
-let editStudentRoutes = require('./routes/editStudent.js')
-app.use('/students/edit/:id', addStudentRoutes)
 
 let teacherRoutes = require('./routes/teacher.js')
 app.use('/teachers', teacherRoutes)
--- routes/student.js
+++ routes/student.js
@@ -1,6 +1,7 @@
 const app = require('express').Router();
 const db = require('../models/index.js')
 
+//Get student data
 app.get('/', (req,res)=>{
     db.Student.findAll().then(students => {
         res.render('../views/student.ejs', {
@@ -8,4 +9,54 @@ app.get('/', (req,res)=>{
         })
     })
 })
+//Get add student data
+app.get('/add',(req,res)=>{
+    res.render('../views/addStudent.ejs')
+})
+app.post('/add', (req,res)=>{
+    db.Student.create({
+        first_name : `${req.body.firstName}`,
+        last_name: `${req.body.lastName}`,
+        email: `${req.body.e_mail}`,
+        createdAt : new Date(),
+        updatedAt : new Date()
+    })
+    res.send('Student has been added successfully <br>'+'<a href="/students">Show All Data</a>')
+})
+
+//get edit student data
+app.get('/edit/:id', (req,res) => {
+    db.Student.findById(req.params.id).then(student => {
+        res.render('../views/editStudent.ejs', {
+            student: student
+        })
+    })
+})
+app.post('/edit/:id', (req,res)=>{
+    db.Student.update(
+        {first_name : `${req.body.firstName}`,
+        last_name :`${req.body.lastName}`,
+        email : `${req.body.e_mail}`},
+        {where: {id: req.params.id} })
+    res.send('Student has been added successfully <br>'+'<a href="/students">Show All Data</a>')
+})
+
+// get delete student data
+app.get('/delete/:id',(req,res) => {
+    db.Student.destroy({where: {id: req.params.id}})
+    res.redirect('/students')
+})
 module.exports = app;
+
+
+// app.get('/', (req,res)=>{
+//     res.render('../views/editStudent.ejs')
+// })
+// app.post('/', (req,res)=>{
+//     let obj = {}
+//     obj[first_name] = `${req.body.firstName}`
+//     db.Student.update(
+//         obj,
+//         {where: {id: itemId} })
+//     res.send('Student has been added successfully <br>'+'<a href="/students">Show All Data</a>')
+// })
