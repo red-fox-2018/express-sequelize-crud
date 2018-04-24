40cab94678a544253667faa1f47d5ae7be78a34d
violerine <viosefferine@gmail.com>
2018-04-24 10:39:41 +0700

    crud

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var express = require('express')
-var app = express()
-let ejs = require('ejs')
-var bodyParser = require('body-parser')
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
 var teacherRoute= require("./routes/teacher-route")
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
-// app.get('/',function(res,res){
-//     res.render('teacher')
-// })
-app.use("/",teacherRoute)
+app.use("/teacher",teacherRoute)
 
-app.listen(5000)
+app.listen(7000, function() {
+    console.log("Running on 7000")
+})
--- routes/teacher-route.js
+++ routes/teacher-route.js
@@ -1,34 +1,54 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 var model = require('../models')
-const ejs = require('ejs');
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json()
+
 var teacher = model.Teacher
 
-router.get('/teacher',function(req,res){
-    // res.send(" KENAPA SIH")
+router.get('/',function(req,res){
     teacher.findAll().then(teachers=>{
         res.render('teacher',{teachers:teachers})
     })
 })
 
-//aaa
-
-router.post('/teacher',function(req,res){
-   var newTeacher = {
-       first : req.body.firstname,
-       last : req.body.lastname
-   }
-   
+router.post('/',function(req,res){
+    console.log("--------tes")
    teacher.create({first_name:req.body.firstname, last_name:req.body.lastname})
+   
    .then(teacher=>{
-    res.render('teacher',{
-        newTeacher:teacher
-    })
+    res.redirect('/teacher')
    })
 })
 
+router.get('/edit/:id',function(req,res){
+    teacher.findById(req.params.id).then(teacher=>{
+        res.render('teacherById',{teacher:teacher})
+    })
+})
 
+router.post('/edit/:id',function(req,res){
+    teacher.update(
+        {
+          first_name:req.body.first_name,
+          last_name:req.body.last_name
+        },
+        {
+          where:{
+              id:Number(req.params.id)
+            }
+        }
+    )
+    .then(function(rowsUpdated){
+        res.redirect('/teacher')
+    })
+})
+ 
+router.get('/delete/:id',function(req,res){
+    teacher.destroy({
+        where:{
+            id:req.params.id
+        }
+    })
+    res.redirect('/teacher')
+})
 
 module.exports = router
